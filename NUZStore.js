e7651fbbe3539c8758cedc90cdcb59c8dbfe6a3e
ZOrion0227 <zorion443@524-mac12538.local>
2017-09-20 17:51:30 -0700

    Added form

--- NUZStore.js
+++ NUZStore.js
@@ -46,7 +46,6 @@ function checkForm() {
 /* Call function to run everytime a
    new item is selected in the lists */
 document.onchange = customChange;
-
 function customChange(){
 //declare variables for body, neck, and bridge
 var body = document.getElementById("bod");
@@ -56,71 +55,20 @@ var bodImage = document.getElementById("body");
 var neckImage = document.getElementById("neck");
 var bridgeImage = document.getElementById("bridge");
 //if statements for neck
-if (neck.value == 1){
-  neckImage.style.backgroundImage = "url('images/WarlockN.png')";
-} else if (neck.value == 2){
-  neckImage.style.backgroundImage = "url('images/SgN.png')";
-} else if (neck.value == 3){
-  neckImage.style.backgroundImage = "url('images/SpikeN.png')";
-} else if (neck.value == 4){
-  neckImage.style.backgroundImage = "url('images/VN.png')";
-} else if (neck.value == 5){
-  neckImage.style.backgroundImage = "url('images/ExplorerN.png')";
-} else if (neck.value == 6){
-  neckImage.style.backgroundImage = "url('images/FancyN.png')";
-} else if (neck.value == 7){
-  neckImage.style.backgroundImage = "url('images/MusicN.png')";
-} else if (neck.value == 8){
-  neckImage.style.backgroundImage = "url('images/ClassicN.png')";
-} else if (neck.value == 9){
-  neckImage.style.backgroundImage = "url('images/TelecasterN.png')";
-} else if (neck.value == 10){
-  neckImage.style.backgroundImage = "url('images/ElectricN.png')";
-} else if (neck.value == 11){
-  neckImage.style.backgroundImage = "url('images/LesPaulN.png')";
-} else if (neck.value == 12){
-  neckImage.style.backgroundImage = "url('images/WesternN.png')";
-} else if (neck.value == 13){
-  neckImage.style.backgroundImage = "url('images/SilverN.png')";
-} else if (neck.value == 14){
-  neckImage.style.backgroundImage = "url('images/TigerN.png')";
-} else if (neck.value == 15){
-  neckImage.style.backgroundImage = "url('images/CemeteryN.png')";
-} else if (neck.value == 0){
+if (neck.value >= 1){
+  neckImage.style.backgroundImage = "url('images/N" + neck.value + ".png')";
+} else{
   neckImage.style.backgroundImage = null;
 }
 //if statements for body
-if (body.value == 1){
-  bodImage.style.backgroundImage = "url('images/WarlockB.png')";
-} else if (body.value == 2){
-  bodImage.style.backgroundImage = "url('images/SgBody.png')";
-} else if (body.value == 3){
-  bodImage.style.backgroundImage = "url('images/SpikeB.png')";
-} else if (body.value == 4){
-  bodImage.style.backgroundImage = "url('images/VB.png')";
-}else if (body.value == 5){
-  bodImage.style.backgroundImage = "url('images/ExplorerB.png')";
-} else if (body.value == 6){
-  bodImage.style.backgroundImage = "url('images/FancyB.png')";
-}else if (body.value == 7){
-  bodImage.style.backgroundImage = "url('images/MusicB.png')";
-}else if (body.value == 8){
-  bodImage.style.backgroundImage = "url('images/ClassicB.png')";
-} else if (body.value == 9){
-  bodImage.style.backgroundImage = "url('images/TelecasterB.png')";
-} else if (body.value == 10){
-  bodImage.style.backgroundImage = "url('images/ElectricB.png')";
-} else if (body.value == 11){
-  bodImage.style.backgroundImage = "url('images/LesPaulB.png')";
-} else if (body.value == 12){
-  bodImage.style.backgroundImage = "url('images/WesternB.png')";
-} else if (body.value == 13){
-  bodImage.style.backgroundImage = "url('images/SilverB.png')";
-} else if (body.value == 14){
-  bodImage.style.backgroundImage = "url('images/TigerB.png')";
-} else if (body.value == 15){
-  bodImage.style.backgroundImage = "url('images/CemeteryB.png')";
-} else if (body.value == 0){
+if (body.value >= 1){
+  bodImage.style.backgroundImage = "url('images/B" + body.value + ".png')";
+}else{
   bodImage.style.backgroundImage = null;
 }
+}
+
+function validateForm(){
+  var fieldsetValidity = true;
+
 }
